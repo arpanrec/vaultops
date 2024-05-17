2ccb036cd0cae61dc1aafcc387f2c992d1b5a27b
arpan <arpan.rec@gmail.com>
2024-05-18 01:07:00 +0530

    fix: allow_any_name in server certificate

--- codifiedvault/pki/server_certificate.tf
+++ codifiedvault/pki/server_certificate.tf
@@ -9,14 +9,14 @@ resource "vault_pki_secret_backend_role" "server_certificate" {
   allow_subdomains         = true
   allow_bare_domains       = true
   allow_glob_domains       = true
-  allow_any_name           = false
+  allow_any_name           = true
   allowed_domains_template = true
   server_flag              = true
   client_flag              = false
   code_signing_flag        = false
   email_protection_flag    = false
   key_type                 = "rsa"
-  key_bits                 = 2048
+  key_bits                 = 4096
   key_usage                = ["DigitalSignature", "KeyAgreement", "KeyEncipherment"]
   ext_key_usage            = ["ExtKeyUsageServerAuth"]
   allow_ip_sans            = true
