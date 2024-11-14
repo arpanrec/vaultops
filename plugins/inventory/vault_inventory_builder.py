8b021e7901ee730bab885a6aee99d911c163b2ef
arpan <arpan.rec@gmail.com>
2024-11-15 01:45:50 +0530

    wip

--- plugins/inventory/vault_inventory_builder.py
+++ plugins/inventory/vault_inventory_builder.py
@@ -78,7 +78,7 @@ class InventoryModule(BaseInventoryPlugin):
     ansible_vault_server_group_name = "vault_vm_servers"
     ansible_vault_node_servers_group_name = "vault_nodes_servers"
 
-    def verify_file(self, path):
+    def verify_file(self, path: str) -> bool:
         """return true/false if this is possibly a valid file for this plugin to consume"""
         valid = False  # this means it will be ignored unless set to True later
         if path.endswith(("inventory.yml", "inventory.yaml")):
@@ -86,8 +86,8 @@ class InventoryModule(BaseInventoryPlugin):
         return valid
 
     def parse(
-        self, inventory, loader, path, cache=True
-    ):  # pylint: disable=too-many-locals,too-many-branches,too-many-statements
+        self, inventory: InventoryData, loader, path: str, cache: bool=True
+    ) -> None:  # pylint: disable=too-many-locals,too-many-branches,too-many-statements
         """parse and populate the inventory with data"""
         super().parse(inventory, loader, path)
         self.loader = loader
